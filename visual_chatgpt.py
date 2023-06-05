bbb0f1529471d092de76b2eae0766b09470d1ed5
Riccardo Giacomelli <riccardo.giacomelli@angloamerican.com>
2023-06-05 20:31:39 +0100

    added makeup-transfer and debug

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -1540,7 +1540,8 @@ class MakeupTransfer:
         image_out = image_out.detach().cpu()
         image_out = asarray(image_out)
         image_out = image_out.squeeze()
-        image_out = np.transpose(image_out, (1, 2, 0))
+        print(f"out image shape {image_out.shape}")
+        #image_out = np.transpose(image_out, (1, 2, 0))
         image_out = Image.fromarray(image_out)
 
         updated_image_path = get_new_image_name(image1_path, func_name=f"makeup-{command}")
