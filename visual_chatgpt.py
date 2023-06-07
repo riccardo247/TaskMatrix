c2940648821b3d04c094eed7b85572aca85f1207
Riccardo Giacomelli <riccardo.giacomelli@angloamerican.com>
2023-06-07 22:07:02 +0100

    fixed r g b values parsing

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -1545,9 +1545,9 @@ class HairColor:
         print(f"{inputs}")
         image1_path = inputs.split(',', 1)[0]
         rgb = inputs.split(',', 1)[1]
-        rgb = rgb.strip("()").split(",")
-        r, g, b = [int(num) for num in rgb]
-        processed = self.pipe2.hair_color(image1_path, (r, g, b)).detach()
+        rgb = rgb.strip().strip("()").split(",")
+        rgb = tuple(int(num) for num in rgb)
+        processed = self.pipe2.hair_color(image1_path, rgb).detach()
 
         updated_image_path = get_new_image_name(image1_path, func_name="hair-segmentation")
         processed = ToTensor()(processed)
