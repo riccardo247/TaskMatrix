c6a933daf2495810f19715be86ac0d49983f43b1
Riccardo Giacomelli <riccardo.giacomelli@angloamerican.com>
2023-06-08 17:12:58 +0100

    corrected  RGB output format

--- Fashion/hairColor.py
+++ Fashion/hairColor.py
@@ -11,7 +11,7 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class HairColor:
-    def __init__(self, load_checkpoint=False, checkpoint_path="hair_segmentation_20epochs.pt"):
+    def __init__(self, load_checkpoint=False, checkpoint_path="hair_segmentation_29"):
         print(f"loading from {checkpoint_path}")
         self.model = model_v0.MobileHairNetV2()
         self.model.load_state_dict(torch.load(checkpoint_path))
@@ -20,7 +20,8 @@ class HairColor:
     def load_image(self, file_path):
         # Open the image file with PIL
         image = cv2.imread(file_path)
-        image = cv2.resize(image, (512, 512))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (224, 224))
 
         return image
 
@@ -51,28 +52,32 @@ class HairColor:
         r, g, b = rgb
         print(f'going to segment image {file_path} and color {rgb}')
         image = self.load_image(file_path)
-        image_tensor = ToTensor()(image)
+        image_norm = image / 255
+        image_tensor = ToTensor()(image_norm)
         print("loaded image")
-        image_tensor = image_tensor.unsqueeze(0)
+        image_tensor = image_tensor.unsqueeze(0).float()
         image_tensor = image_tensor.to(DEVICE)
         print(f"starting inference")
         mask_out = self.model(image_tensor)
         mask_out = mask_out.squeeze(0)
         mask_out = torch.cat([mask_out, mask_out, mask_out], dim=0)
         mask_out = mask_out.detach().to('cpu').numpy()
+        mask_out = mask_out * 255
+        mask_out = mask_out.astype(np.uint8)
         mask_out = mask_out.transpose(1, 2, 0)
-        mask_out = mask_out  *255
         lab_img = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
         L, A, B = cv2.split(lab_img)
         B, G, R = cv2.split(image)
-        B[mask_out[:, :, 0] >= 150] = b
-        G[mask_out[:, :, 0] >= 150] = g
-        R[mask_out[:, :, 0] >= 150] = r
+        B[mask_out[:, :, 0] >= 180] = b
+        G[mask_out[:, :, 0] >= 180] = g
+        R[mask_out[:, :, 0] >= 180] = r
 
         colored = cv2.merge([B, G, R])
         L1, A1, B1 = cv2.split(cv2.cvtColor(colored, cv2.COLOR_BGR2LAB))
         processed = cv2.merge([L, A1, B1])
         processed = cv2.cvtColor(processed, cv2.COLOR_LAB2BGR)
+        # back to RGB
+        processed = processed[..., ::-1].copy()
         print(f"returned processed")
 
         return processed
